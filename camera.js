23bd754a79f41c1f516bbee92275266341d1febe
AdriansSB <92295503+AdriansSB@users.noreply.github.com>
2021-10-11 11:10:12 +0300

    update

--- camera.js
+++ camera.js
@@ -1,27 +1,3 @@
-// var video = document.getElementById("video");
-
-
-        // function on_cam_success(stream){
-                // video.srcObject = stream;
-// }
-
-        // function on_cam_error(err){
-                // alert("error."+err.message);
-// }
-
-        // var constraints = { audio : false, video : true} ;
-        // navigator.mediaDevices.getUserMedia(constraints)
-                // .then(on_cam_success)
-                // .catch(on_cam_error);
-
-        // function capteaza()
-        // {
-            // var c = document.getElementById("canvas");
-            // c.width = video.width;
-            // c.height = video.height;
-            // var ctx = c.getContext("2d");
-            // ctx.drawImage(video, 0, 0, 640, 480);
-        // }
 		
 var video = document.getElementById("video");
 //-----------------------------------------------
@@ -42,4 +18,7 @@ function on_cam_error(err){
 		c.height = video.height;
 var ctx = c.getContext("2d");ctx.drawImage(video, 0, 0, 640, 480);
 }
+
+video.addEventListener("touchstart",capteaza);
+video.addEventListener("mousedown",capteaza);
 //-----------------------------------------------
